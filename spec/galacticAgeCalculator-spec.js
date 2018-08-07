0f5d6f0c128248e89084d8222108abae535c37a1
Jean Jia <jean84646@gmail.com>
2018-08-06 23:28:10 -0700

    Modify test parameters. Modify methods to use this. Refactor expectToLive loop.

--- spec/galacticAgeCalculator-spec.js
+++ spec/galacticAgeCalculator-spec.js
@@ -2,45 +2,86 @@ import { galacticAgeCalculator } from './../src/galacticAgeCalculator.js';
 
 describe('galacticAgeCalculator', function() {
   let testUser;
+  let testUserTwo;
 
   beforeEach(function() {
-    testUser = new galacticAgeCalculator(1, "2018-8-2");
+    testUser = new galacticAgeCalculator(10, "2008/08/03", 100);
+    testUserTwo = new galacticAgeCalculator(110, "2008-08-03", 100);
   });
 
   it('should convert age in years into seconds', function() {
-    expect(testUser.ageInSeconds()).toEqual(31536000);
+    expect(testUser.ageInSeconds()).toEqual(315360000);
   });
 
-  // it('should find the difference between two dates in seconds', function() {
-  //   let currentDate = new Date();
-  //   expect(compareDate(testUser.birthday, currentDate).toEqual(31536000);
-  // });
-
-  it('should find age in Mercury years in seconds', function() {
-    expect(testUser.mercuryYears()).toEqual(.24);
+  it('should find the difference between two dates in seconds', function() {
+    let testDate1 = new Date("2008/08/05");
+    expect(testUser.compareDateInSeconds(testDate1)).toEqual(172800);
   });
 
-  it('should find age in Venus years in seconds', function() {
-    expect(testUser.venusYears()).toEqual(.62);
+  it('should find age on Mercury', function() {
+    expect(testUser.mercuryAge()).toEqual(41);
   });
 
-  it('should find age in Mars years in seconds', function() {
-    expect(testUser.marsYears()).toEqual(1.88);
+  it('should find age on Venus', function() {
+    expect(testUser.venusAge()).toEqual(16);
   });
 
-  it('should find age in Jupiter years in seconds', function() {
-    expect(testUser.jupiterYears()).toEqual(11.86);
+  it('should find age on Mars', function() {
+    expect(testUser.marsAge()).toEqual(5);
   });
 
-  it('should find years left to live', function() {
-    let lifeExpectancy = 100;
-    expect(testUser.expectToLive(lifeExpectancy)).toEqual(99);
+  it('should find age on Jupiter', function() {
+    expect(testUser.jupiterAge()).toEqual(0);
   });
 
-  it('should find years exceeds life expectancy', function() {
-    let testUserTwo = new galacticAgeCalculator(101, "1918-8-3");
-    let lifeExpectancy = 100;
-    expect(testUserTwo.expectToLive(lifeExpectancy)).toEqual(1);
+  it('should find years left to live on earth', function() {
+    let location = "earth";
+    expect(testUser.expectToLive(location)).toEqual(90);
+  });
+
+  it('should find years exceeds life expectancy on earth', function() {
+    let location = "earth";
+    expect(testUserTwo.expectToLive(location)).toEqual(10);
+  });
+
+  it('should find years left to live on mercury', function() {
+    let location = "mercury";
+    expect(testUser.expectToLive(location)).toEqual(375);
+  });
+
+  it('should find years exceeds life expectancy on mercury', function() {
+    let location = "mercury";
+    expect(testUserTwo.expectToLive(location)).toEqual(42);
+  });
+
+  it('should find years left to live on venus', function() {
+    let location = "venus";
+    expect(testUser.expectToLive(location)).toEqual(145);
+  });
+
+  it('should find years exceeds life expectancy on venus', function() {
+    let location = "venus";
+    expect(testUserTwo.expectToLive(location)).toEqual(16);
+  });
+
+  it('should find years left to live on mars', function() {
+    let location = "mars";
+    expect(testUser.expectToLive(location)).toEqual(48);
+  });
+
+  it('should find years exceeds life expectancy on mars', function() {
+    let location = "mars";
+    expect(testUserTwo.expectToLive(location)).toEqual(5);
+  });
+
+  it('should find years left to live on jupiter', function() {
+    let location = "jupiter";
+    expect(testUser.expectToLive(location)).toEqual(8);
+  });
+
+  it('should find years exceeds life expectancy on jupiter', function() {
+    let location = "jupiter";
+    expect(testUserTwo.expectToLive(location)).toEqual(1);
   });
 
 });
--- src/galacticAgeCalculator.js
+++ src/galacticAgeCalculator.js
@@ -1,36 +1,51 @@
 export class galacticAgeCalculator {
-  constructor(age, birthday) {
+  constructor(age, birthday, lifeExpectancy) {
     this.age = age;
     this.birthday = birthday;
+    this.lifeExpectancy = lifeExpectancy;
   }
 
   ageInSeconds() {
-    debugger;
     return this.age * 365 * 24 * 3600;
   }
 
-  mercuryYears() {
-    return this.age * .24;
+  compareDateInSeconds(date1) {
+    let date2 = new Date(this.birthday);
+    return Math.abs(date1 - date2) / 1000;
   }
 
-  venusYears() {
-    return this.age * .62;
+  mercuryAge() {
+    return Math.floor(this.age / .24);
   }
 
-  marsYears() {
-    return this.age * 1.88;
+  venusAge() {
+    return Math.floor(this.age / .62);
   }
 
-  jupiterYears() {
-    return this.age * 11.86;
+  marsAge() {
+    return Math.floor(this.age / 1.88);
   }
 
-  expectToLive(lifeExpectancy) {
-    if (this.age < lifeExpectancy) {
-      return lifeExpectancy - this.age;
+  jupiterAge() {
+    return Math.floor(this.age / 11.86);
+  }
+
+  expectToLive(location) {
+  const mercuryLifeExpectancy = Math.floor(this.lifeExpectancy / .24);
+  const venusLifeExpectancy = Math.floor(this.lifeExpectancy / .62);
+  const marsLifeExpectancy = Math.floor(this.lifeExpectancy / 1.88);
+  const jupiterLifeExpectancy = Math.floor(this.lifeExpectancy / 11.86);
+
+    if (location === "earth") {
+        return Math.abs(this.lifeExpectancy - this.age);
+    } else if (location === "mercury") {
+        return Math.abs(mercuryLifeExpectancy - this.mercuryAge());
+    } else if (location === "venus") {
+        return Math.abs(venusLifeExpectancy - this.venusAge());
+    } else if (location === "mars") {
+        return Math.abs(marsLifeExpectancy - this.marsAge());
     } else {
-      return this.age - lifeExpectancy;
+        return Math.abs(jupiterLifeExpectancy - this.jupiterAge());
     }
   }
-
 }
